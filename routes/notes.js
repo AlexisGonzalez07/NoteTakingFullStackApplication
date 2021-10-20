41d863b831963a2b67f226a663f66daacc9cabdc
AlexisGonzalez07 <alexis.gonzalez07@gmail.com>
2021-10-20 05:41:31 -0700

    Added helper functions, added comments and pseudocode

--- routes/notes.js
+++ routes/notes.js
@@ -1,14 +1,14 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
-// GET Route for retrieving all the tips
+// GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
   console.info(`${req.method} request received for notes`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for a new UX/UI tip
+// POST Route for a new UX/UI note
 notes.post('/', (req, res) => {
   console.info(`${req.method} request received to add a note`);
   console.log(req.body);
@@ -28,5 +28,25 @@ notes.post('/', (req, res) => {
     res.json('Error in adding Note');
   }
 });
+
+// Delete Route for removing a note
+notes.delete('/:id', (req, res) => {
+    console.info(`${req.method} requrest received for notes`);
+    console.log(req.body);
+
+    const {title, text, note_id} = req.body
+
+    if (title && text && note_id) {
+        // Creating a variable to be compared to
+        const currentNote = {
+            title,
+            text,
+            note_id
+        };       //write a function below to read the file, loop and find the id, and then delete the note 
+
+    }
+    
+}
+)
 
 module.exports = notes;
